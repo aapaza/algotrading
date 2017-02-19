a366bcb66a0f0908af8f8be94b18af875aa60c39
Fabian Orccon <cfoch.fabian@gmail.com>
2017-02-19 03:04:08 -0500

    Add scraping utils. Add SP500_tickers_from_wikipedia

---
+++ src/scraping.py
@@ -0,0 +1,35 @@
+# Algotrading
+#
+#       src/scraping.py
+#
+# Copyright (c) 2017, Fabian Orccon <cfoch.fabian@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+
+import bs4
+import pickle
+import requests
+
+def SP500_tickers_from_wikipedia():
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.text)
+    table = soup.find("table", {"class": "wikitable sortable"})
+    tickers = []
+    for row in table.findAll("tr")[1:]:
+        ticker = row.findAll("td")[0].text
+        tickers.append(ticker)
+    return tickers
